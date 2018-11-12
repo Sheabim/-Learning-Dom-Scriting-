3fe020d965165b7838ad26fa8aa077757e708eee
shailabrazil <sheilaadesina85@gmail.com>
2018-11-12 15:33:47 +1100

    s

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
-
 const mainDiv = document.querySelector('.main');
-
 const ul = document.getElementById('invitedList');
 const div = document.createElement('div');
 const filterLabel = document.createElement('label');
